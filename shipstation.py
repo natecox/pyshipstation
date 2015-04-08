3437d8aa778ed7d8482d6d084e934affde4250d2
Nathan Cox <akujin@akujin.com>
2015-04-07 18:18:34 -0700

    updated #add_item to properly create a dictionary

--- shipstation.py
+++ shipstation.py
@@ -1,10 +1,36 @@
 from django.conf import settings
 import requests
 
+
+class ShipStationOrder:
+    """
+    Accepts the data needed for an individual ShipStation order and
+    contains the tools for submitting the order to ShipStation.
+    """
+
+    def __init__(self):
+        self.items = []
+        self.container = None
+
+    def add_item(self, key=None, sku=None, name=None, image_url=None,
+                 weight=None, quantity=None, unit_price=None,
+                 warehouse_location=None, options=None):
+        """
+        Adds a new item to the order with all of the required keys.
+        """
+        details = locals()
+        details.pop('self')
+        self.items.append(details)
+
+    def get_items(self):
+        return self.items
+
+
 class ShipStation:
     """
     Handles the details of connecting to and querying a ShipStation account.
     """
+
     def __init__(self, key=None, secret=None):
         """
         Connecting to ShipStation required an account and a
@@ -26,6 +52,14 @@ class ShipStation:
                 raise AttributeError('Key must be supplied.')
         else:
             self.secret = secret
+
+        self.orders = []
+
+    def add_order(self, order):
+        self.orders.append(order)
+
+    def get_orders(self):
+        return self.orders
 
     def get(self, endpoint):
         url = '{}{}'.format(self.url, endpoint)
