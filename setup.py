75e6c3f21ff95b87660e0bd32c3ae3a5bb0f21e4
Nathan Cox <akujin@akujin.com>
2015-05-29 18:21:24 -0700

    version bump for debug test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='shipstation',
-    version='0.0.611',
+    version='0.0.612',
     author='Nathan Cox',
     author_email='akujin@akujin.com',
     classifiers=[
