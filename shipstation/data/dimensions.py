135e1ecef4bb6320abb0bb1efe4e0b7019e9db70
Mike Rivera <rivera.mj@gmail.com>
2019-03-17 18:14:42 -0400

    Dimension checks and test

--- shipstation/data/dimensions.py
+++ shipstation/data/dimensions.py
@@ -1,14 +1,27 @@
 from shipstation.data.base import ShipStationBase
 from shipstation.data.weight import ShipStationWeight
 
+from decimal import Decimal
+
 
 class ShipStationContainer(ShipStationBase):
-    def __init__(self, units=None, length=None, width=None, height=None):
+    def __init__(self, units, length, width, height):
         self.units = units
         self.length = length
         self.width = width
         self.height = height
         self.weight = None
+
+        if not units:
+            raise AttributeError('Units must not be empty')
+        if units not in ['inches', 'centimeters']:
+            raise AttributeError('Units must be `inches` or `centimeters`')
+        if not isinstance(length, (Decimal, int, float)):
+            raise AttributeError('Length must be a number')
+        if not isinstance(width, (Decimal, int, float)):
+            raise AttributeError('Length must be a number')
+        if not isinstance(height, (Decimal, int, float)):
+            raise AttributeError('Length must be a number')
 
     def set_weight(self, weight):
         if type(weight) is not ShipStationWeight:
